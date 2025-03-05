f7e9242b978af484745afb362a52ec2c32139e7e
WHUASHAI <jamesbaloro99@gmail.com>
2025-03-05 20:42:53 +0800

    updated student routes

--- src/student.routes.ts
+++ src/student.routes.ts
@@ -8,7 +8,7 @@ import express, { Request, Response } from "express";
 import { Gender } from "./_helpers/gender";
 const studentRouter = express.Router();
 
-studentRouter.get("/users", async (req: Request, res: Response) => {
+studentRouter.get("/Student", async (req: Request, res: Response) => {
   try {
     const students = await AppDataSource.manager.find(Student);
 
@@ -28,20 +28,20 @@ studentRouter.get("/students/:id", async (req: Request, res: Response) => {
     const studentID = Number(req.params.id);
 
     if (isNaN(studentID)) {
-      return res.status(400).json({ msg: "invalid user id" });
+      return res.status(400).json({ msg: "invalid Student id" });
     }
 
-    const user = await AppDataSource.manager.findOneBy(Student, {
+    const student = await AppDataSource.manager.findOneBy(Student, {
       id: studentID,
     });
 
-    if (!user) {
-      return res.status(404).json({ msg: `user id: ${studentID} cant be found` });
+    if (!studentID) {
+      return res.status(404).json({ msg: `Student id: ${studentID} cant be found` });
     }
 
-    return res.status(200).json({ msg: "User found", user });
+    return res.status(200).json({ msg: "Student found", student });
   } catch (err) {
-    console.error("Error fetching user:", err);
+    console.error("Error fetching Student:", err);
     return res.status(500).json({ msg: "Internal server error" });
   }
 });
@@ -82,7 +82,7 @@ studentRouter.post("/students", async (req: Request, res: Response) => {
 });
 
 //boss carl
-studentRouter.put("/user/:id", async (req: Request, res: Response) => {
+studentRouter.put("/Students/:id", async (req: Request, res: Response) => {
   try {
     const StudentRepository = AppDataSource.getRepository(Student);
     const StudentID = Number(req.params.id);
@@ -130,7 +130,7 @@ studentRouter.put("/user/:id", async (req: Request, res: Response) => {
 
     return res
       .status(200)
-      .json({ message: `User ${StudentID} updated successfully` });
+      .json({ message: `Student ${StudentID} updated successfully` });
   } catch (err) {
     console.error("Error updating user:", err);
     return res.status(500).json({ error: "Internal server error" });
